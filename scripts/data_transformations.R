0a6edf3d72631d8c2a52abca5ff24ae81ac591bf
Sylvester de Koning <sylvester.de.koning90@gmail.com>
2016-06-20 14:37:57 +0200

    explorations

---
+++ scripts/data_transformations.R
@@ -0,0 +1,32 @@
+# Checking for data.
+
+if(!file.exists("./data/train.csv")){
+    print("not all files present, please refer to the kaggle site for the data.")
+    browseURL("https://www.kaggle.com/c/integer-sequence-learning/data")
+    
+} else if(!file.exists("./data/train.csv") & file.exists("./data/train.csv.zip")) {
+    print("unzipping file...")
+    unzip("./data/test.csv.zip", exdir = "./data")
+    unzip("./data/train.csv.zip", exdir = "./data")
+    unzip("./data/sample_submission.csv.zip", exdir = "./data")
+} else {
+    print("reading in data...")
+    test.data <- read.csv("./data/test.csv")
+    train.data <- read.csv("./data/train.csv")
+    sample.submission <- read.csv("./data/sample_submission.csv")
+}
+
+# Dependencies.
+library(dplyr)
+library(stringr)
+library(readr)
+
+# Setting seed and options.
+set.seed(20062016)
+options(scipen = 999)
+
+# Function for splitting strings in a list. 
+ParseSequence <- function(str) {
+    return(as.numeric(str_split(str, ",")[[1]]))
+}
+
